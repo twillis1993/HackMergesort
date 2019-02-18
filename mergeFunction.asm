d4d20333da68a88dcddb9fa557d20981b11fb5dc
Tom Willis <tom.willis@live.com>
2019-02-18 20:26:00 +0000

    Added code to copy what's left of arrays, but not working at the moment

--- mergeFunction.asm
+++ mergeFunction.asm
@@ -166,6 +166,14 @@ M=D
 //To merge, need source addresses (2000 and 3000), length of each array, first address of first array
 // Current state of stack: [BASE] first address of first array, length of first array [TOP]
 // Length of second array is in R5; TODO tidy this up with stack-based or register-based, but not both
+// R1 = address of first element of first array
+// R2 = length of first array
+// R3 = length of second array
+// R4 = index into first array
+// R5 = index into second array
+// R6 = current element in first array
+// R7 = current element in second array
+// R8 = destination address
 // Pop and save length of first array in R2
 @0
 A=M
@@ -280,7 +288,80 @@ M=M+1
 @MERGE_LOOP
 0; JMP
 (END_MERGE_LOOP)
-// handle case where do not interleave arrays
+// if first array still has elements, copy, otherwise copy rest of array 2 elements
+@2
+D=M
+D=D-1
+@4
+D=D-M
+@COPY_2_LAST
+D; JEQ
+// still elements in first array, copy the rest of it, else copy second array (COPY_2_LAST)
+(COPY_1_LAST)
+// check whether there are any elements in first array left to copy
+@2
+D=M
+D=D-1
+@4
+D=D-M
+@RET_MERGE
+D; JEQ
+// copying element of first array: get index into destination array as sum of indices into each of the two arrays being merged
+@4
+D=M
+@5
+D=D+M
+// D is now index into destination array. Add to address of first element to set D to destination address
+@1
+D=D+M
+// Save destination address in R8
+@8
+M=D
+// Get current element of first array in D
+@6
+D=M
+// Write to destination address
+@8
+A=M
+M=D
+// Increment index into copy of first array
+@4
+M=M+1
+@COPY_1_LAST
+0; JMP
+// else copy what is left of second array
+(COPY_2_LAST)
+// check whether there are any elements in first array left to copy
+@3
+D=M
+D=D-1
+@5
+D=D-M
+@RET_MERGE
+D; JEQ
+// copying element of second array: get index into destination array as sum of indices into each of the two arrays being merged
+@4
+D=M
+@5
+D=D+M
+// D is now index into destination array. Add to destination address to set D to destination address
+@1
+D=D+M
+// Save destination address in R8
+@8
+M=D
+// Get current element of second array in D
+@7
+D=M
+// Write to destination address
+@8
+A=M
+M=D
+// Increment index into copy of second array
+@5
+M=M+1
+@COPY_2_LAST
+0; JMP
 @RET_MERGE
 0; JMP
 //////
