0d6599cdb64f49776ca082c4eba9aba3c2d0b69f
Tom Willis <tom.willis@live.com>
2019-02-18 19:39:34 +0000

    Committing and testing before handling the case where the arrays are not interleaved by MERGE

--- mergeFunction.asm
+++ mergeFunction.asm
@@ -5,12 +5,9 @@
 // Reserve 1-15 as virtual registers
 // Push function return address 
 //////////////////////////////
-// TODO is copying a and b to 2000 and 3000 ok?
 // TODO limited to arrays of length 1000 by the temporary memory used in copy
-// TODO what information does merge need once both copies return?
-//	destination address (address of first element of first array)
-// 	length of merged array	
 // TODO compute/save length of array, destination address online function body rather than pushing first address twice
+// TODO resolve having first array arguments on stack and length second in R5
 /////////////////////////////
 // Push return address
 @0
@@ -20,17 +17,7 @@ D=A
 @0
 A=M
 M=D
-// Push address of length of merged array
-@0
-M=M+1
-@17
-D=M
-@19
-D=D+M
-@0
-A=M
-M=D
-// Push address of first element array TODO don't pop me in second copy call
+// Push address of first element array
 @0
 M=M+1
 @16
@@ -74,17 +61,6 @@ M=D
 // MERGE
 // Requires operands on the stack
 (MERGE)
-// Clear virtual registers
-//@1
-//M=0
-//@2
-//M=0
-//@3
-//M=0
-//@4
-//M=0
-//@5
-//M=0
 // Pop and save length of second array in R5
 @0
 A=M
@@ -137,33 +113,19 @@ M=D
 @COPY
 0; JMP
 (RET_COPY_ONE)
-// Clear virtual registers
-//@1
-//M=0
-//@2
-//M=0
-//@3
-//M=0
-//@4
-//M=0
-//@5
-//M=0
-// Save length of first array in R3
+// Save (but do not pop as the merging requires it) length of first array in R3
 @0
 A=M
 D=M
 @3
 M=D
+// Save (but do not pop as the merging requires it) address of first element of first array in R2
+// Peek at penultimate element
 @0
-M=M-1
-// Save address of first element of first array in R2
-@0
-A=M
+A=M-1
 D=M
 @2
 M=D
-@0
-M=M-1
 // COPY to 2000 onwards
 // Push function return address
 @0
@@ -202,22 +164,129 @@ M=D
 (RET_COPY_TWO)
 // Arrays copied, time to merge
 //To merge, need source addresses (2000 and 3000), length of each array, first address of first array
-//
+// Current state of stack: [BASE] first address of first array, length of first array [TOP]
+// Length of second array is in R5; TODO tidy this up with stack-based or register-based, but not both
+// Pop and save length of first array in R2
+@0
+A=M
+D=M
+@2
+M=D
+@0
+M=M-1
+// Pop and save address of first element of first array in R1 // TODO this was labelled as 1st of 2nd array, make sure there's not a bug here
+@0
+A=M
+D=M
+@1
+M=D
+@0
+M=M-1
+// Move length of second array from R5 to R3
+@5
+D=M
+@3
+M=D
+// Index into first array in R4
+@4
+M=0
+// Index into second array in R5
+@5
+M=0
+// Merge arrays
+(MERGE_LOOP)
+// Check for termination condition for first array
+@4
+D=M+1
+@2
+@END_MERGE_LOOP
+M-D; JEQ
+// Check for termination condition for second array
+@5
+D=M+1
+@3
+@END_MERGE_LOOP
+M-D; JEQ
+// else compare current elements
+// current element in first array in R6
+@4
+D=M
+@2000
+A=A+D
+D=M
+@6
+M=D
+// current element in second array R7
+@5
+D=M
+@3000
+A=A+D
+D=M
+@7
+M=D
+// compare current elements
+@7
+D=M
+@6
+D=D-M
+// if D >0, copy second, else copy first, then increment relevant index
+@COPY_2_EL
+D; JLT
+// copying element of first array: get index into destination array as sum of indices into each of the two arrays being merged
+@4
+D=M
+@5
+D=D+M
+// D is now index into destination array. Add to address of first element to set D to destination address
+@1
+D=D+M
+// Save destination address in R8
+@8
+M=D
+// Get current element of first array in D
+@6
+D=M
+// Write to destination address
+@8
+A=M
+M=D
+// Increment index into copy of first array
+@4
+M=M+1
+@MERGE_LOOP
+0; JMP
+(COPY_2_EL)
+// copying element of second array: get index into destination array as sum of indices into each of the two arrays being merged
+@4
+D=M
+@5
+D=D+M
+// D is now index into destination array. Add to destination address to set D to destination address
+@1
+D=D+M
+// Save destination address in R8
+@8
+M=D
+// Get current element of second array in D
+@7
+D=M
+// Write to destination address
+@8
+A=M
+M=D
+// Increment index into copy of second array
+@5
+M=M+1
+@MERGE_LOOP
+0; JMP
+(END_MERGE_LOOP)
+// handle case where do not interleave arrays
 @RET_MERGE
 0; JMP
 //////
 // COPY
 // Requires operands on the stack
 (COPY)
-// Clear virtual registers
-//@1
-//M=0
-//@2
-//M=0
-//@3
-//M=0
-//@4
-//M=0
 // Save destination address in R4
 @0
 A=M
