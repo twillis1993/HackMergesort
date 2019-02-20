eb3da5588946942c9e21f1fa93de2921c82aae39
Tom Willis <tom.willis@live.com>
2019-02-20 16:15:16 +0000

    Test script written. Just need to write readme and tidy up code comments.

--- python/generateTestScript.py
+++ python/generateTestScript.py
@@ -5,9 +5,9 @@ import random
 def configureArgumentParser():
 	parser = argparse.ArgumentParser()
 
-	parser.add_argument("-A", "--asmName", help="Name for the assembly file to run", type=str, required=True)
+	parser.add_argument("-A", "--asmName", help="Name for the assembly file to run.", type=str, required=True)
 
-	parser.add_argument("-N", "--outputName", action=OutputNameAction, help="Name for the .tst script", type=str, default="myTestScript.tst")
+	parser.add_argument("-O", "--outputName", action=OutputNameAction, help="Name for the .tst script", type=str, default="myTestScript.tst")
 
 	inputGroup = parser.add_mutually_exclusive_group(required=True)
 
@@ -17,9 +17,26 @@ def configureArgumentParser():
 
 	return parser
 
-def writeTstScript(asmName, testScriptName):
-	with open(testScriptName, 'w') as fh:
-		pass
+def writeTstScript(argsDict):
+	with open(".".join([argsDict["outputName"], "tst"]), 'w') as fh:
+		fh.write("load %s,\n" % argsDict["asmName"])
+		#fh.write("output-file %s,\n" % ".".join([argsDict["outputName"], "out"]))
+		#fh.write("compare-to %s,\n" % ".".join([argsDict["outputName"], "cmp"]))
+		#fh.write("output-list %s,\n" % " ".join(["RAM[%d]%%D2.6.2" % x for x in range(4000, 4000+len(argsDict["input"]))]))
+
+		fh.write("breakpoint PC 3,\n")
+		fh.write("\n")
+		fh.write("set RAM[101] 2,\n")
+		fh.write("set RAM[102] 4000,\n")
+		fh.write("set RAM[103] %d,\n" % len(argsDict["input"]))
+		fh.write("\n")
+		fh.write("set RAM[0] 103,\n")
+		fh.write("\n")
+		for i, x in enumerate(argsDict["input"]):
+			fh.write("set RAM[%d] %d,\n" % (4000+i, argsDict["input"][i]))
+
+		fh.write("repeat {\n\tticktock;\n}\n")
+
 		
 class OutputNameAction(argparse.Action):
 	def __call__(self, parser, namespace, values, option_string):
@@ -33,4 +50,5 @@ class GenerateRandomInputAction(argparse.Action):
 if __name__  == "__main__":
 	parser = configureArgumentParser()
 	args = parser.parse_args()
-	print vars(args)["input"]
+	writeTstScript(vars(args))
+
